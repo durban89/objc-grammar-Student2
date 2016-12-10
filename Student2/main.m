f69595372e63fe34236bdfd69cef059b513b9558
dapeng126 <zhangdapeng89@126.com>
2016-12-10 12:38:58 +0800

    Initial Commit

---
+++ Student2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Student2
+//
+//  Created by durban on 16/12/10.
+//  Copyright © 2016年 durban126. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
